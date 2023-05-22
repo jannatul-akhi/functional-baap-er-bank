9287974e75df0737d7edc445e9e63d88e2739b46
jannatul-akhi <jannatulferdous3608@gmail.com>
2023-05-22 23:53:14 +0600

    baap er bank practice

---
+++ js/withdraw.js
@@ -0,0 +1,23 @@
+/**
+ * DRY - Do Not Repeat yourself
+ * add withdraw button event handler
+ * get withdraw amount by using getInputFieldValueById function
+ * get previous withdraw amount by using getTextElementValueById function
+ * calculate new withdraw total and set the value
+ * set new withdraw total by using setTextElementValueById
+ * get previous balance total
+ * calculate new total balance
+ * set the total balance
+ */
+
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    const newWithdrawAmount = getInputFieldValueById('withdraw-field');
+    const previousWithdrawTotal = getTextElementValueById('withdraw-total');
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    setTextElementValueById('withdraw-total', newWithdrawTotal);
+
+    const previousBalanceTotal = getTextElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    setTextElementValueById('balance-total', newBalanceTotal);
+})
